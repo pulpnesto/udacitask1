292c322a0f4efd10ec67439f3a9093134fad0ad0
pulpnesto <pulp.nesto@gmail.com>
2016-01-31 12:52:06 -0700

    Comment code. Finish menu.rb. Describe new features in new_features.txt

--- menu.rb
+++ menu.rb
@@ -1,16 +1,22 @@
 require_relative "todolist"
 
+
+
 class Menu
+	# Initialize menu with a new untitled to-do list
 	def initialize
 		@todo_list = TodoList.new("Untitled To-Do List")
 	end
 
+	# Clear screen and print to-do list and menu options
+	# This works on a Mac but have not tested on a Windows terminal
 	def print_menu
 		system ('cls') or system ('clear')
 		@todo_list.print_list
 		print_menu_options
 	end
 
+	# Prints the menu options and asks for a user choice
 	def print_menu_options
 		puts "----------------------------------------------"
 		puts "1) Rename List       |      2) Add task  "
@@ -21,13 +27,16 @@ class Menu
 		puts "----------------------------------------------"
 		puts "Enter choice: "
 		choice = get_choice.to_i
-		options(choice)
+		options choice
 	end
 
+	# Gets users input
 	def get_choice
 		gets.chomp
 	end
 
+	# Probably a better way to do this,
+	# but just routes user's menu selection to the right submenu
 	def options(choice)
 		if choice == 1
 			rename_submenu
@@ -52,6 +61,7 @@ class Menu
 		end
 	end
 
+	# Add a due date, reprints the main menu when done
 	def due_date_submenu
 		puts "Enter number of task to add a due date:"
 		choice = get_choice.to_i
@@ -61,64 +71,67 @@ class Menu
 		print_menu
 	end
 
+	# Remove due date, reprints the main menu when done
 	def remove_date_submenu
 		puts "Enter number of task to remove due date:"
 		choice = get_choice.to_i
-		@todo_list.remove_due_date(choice)
+		@todo_list.remove_due_date choice
 		print_menu
 	end
 
-
+	# Prioritize task, reprints the main menu when done
 	def prioritize_submenu
 		puts "Enter number of task to prioritize:"
 		choice = get_choice.to_i
-		@todo_list.prioritize_item(choice)
+		@todo_list.prioritize_item choice
 		print_menu
 	end
 
+	# Remove priority flag, reprints the main menu when done
 	def normalize_submenu
 		puts "Enter number of task to normalize:"
 		choice = get_choice.to_i
-		@todo_list.normalize_item(choice)
+		@todo_list.normalize_item choice
 		print_menu
 	end
 
-
+	# Flag task as completed, reprints the main menu when done
 	def complete_submenu
 		puts "Enter number of task to complete:"
 		choice = get_choice.to_i
-		@todo_list.complete_item(choice)
+		@todo_list.complete_item choice
 		print_menu
 	end
 
+	# Removes complete flag for task, reprints the main menu when done
 	def uncomplete_submenu
 		puts "Enter number of task to uncomplete:"
 		choice = get_choice.to_i
-		@todo_list.uncomplete_item(choice)
+		@todo_list.uncomplete_item choice
 		print_menu
 	end
 
+	# Adds a new task to list, reprints the main menu when done
 	def add_task_submenu
 		puts "Enter task to add to list:"
 		choice = get_choice
-		@todo_list.add_item(choice)
+		@todo_list.add_item choice
 		print_menu
 	end
 
+	# Removes task from list, reprints the main menu when done
 	def delete_task_submenu
 		puts "Enter number of task to remove:"
 		choice = get_choice.to_i
-		@todo_list.remove_item(choice)
+		@todo_list.remove_item choice
 		print_menu
 	end
 
+	# Allows user to rename to-do list, reprints the main menu when done
 	def rename_submenu
 		puts "Enter new list name:"
 		choice = get_choice
-		@todo_list.rename_title(choice)
+		@todo_list.rename_title choice
 		print_menu
 	end
-
-
-
 end
--- todolist.rb
+++ todolist.rb
@@ -17,50 +17,58 @@ class TodoList
 
   ## Add an Item to the array of Items
   def add_item(new_item)
-  	item = Item.new(new_item)
+  	item = Item.new new_item
   	@items.push item
   end
 
+  ## Remove a taks from list
   def remove_item(item_id)
-  	@items.delete_at(item_id-1)
+  	@items.delete_at item_id-1
   end
 
+  ## Flag the task as completed
   def complete_item(item_id)
-  	if indexed?(item_id)
+  	if indexed? item_id
   		@items[item_id-1].complete_item
   	end
   end
 
+  ## Remove completed flag from the task
   def uncomplete_item(item_id)
-  	if indexed?(item_id)
+  	if indexed? item_id
   		@items[item_id-1].uncomplete_item
   	end
   end
 
+  ## Flag the task as a priority
 	def prioritize_item(item_id)
-		if indexed?(item_id)
+		if indexed? item_id
 			@items[item_id-1].make_priority
 		end
 	end
 
+	## Remove priority flag from task
 	def normalize_item(item_id)
-		if indexed?(item_id)
+		if indexed? item_id
 			@items[item_id-1].remove_priority
 		end
 	end
 
+	## Add due date to task using yy-mm-dd format
 	def add_due_date(item_id, due_date)
-		if indexed?(item_id)
-			@items[item_id-1].set_due_date(due_date)
+		if indexed? item_id
+			@items[item_id-1].set_due_date due_date
 		end
 	end
 
+	## Remove due date from task
 	def remove_due_date(item_id)
-		if indexed?(item_id)
+		if indexed? item_id
 			@items[item_id-1].remove_date
 		end
 	end
 
+	## Check to see if task id provided by user is in the range of the items array
 	def indexed?(item_id)
 		size_of_index = @items.length
 		if item_id > size_of_index || item_id < 1
@@ -70,8 +78,9 @@ class TodoList
 		end
 	end
 
+	## Return the index of the task
   def get_item_index(item)
-  	@items.index(item)
+  	@items.index item
   end
 
   ## Print the title and call the Item print method
@@ -80,57 +89,69 @@ class TodoList
   	puts "#{@title}"
   	puts "----------------------------------------------"
    	@items.each do |item|
-  		item_index = get_item_index(item)
+  		item_index = get_item_index item
   		item.print_item(item_index + 1)
   	end
 	end
-
 end
 
+
+
 class Item
+	# Not sure of where to place this require...so I put it here and it works!
 	require 'date'
   # methods and stuff go here
   attr_accessor :description, :completed_status, :priority, :due_date
 
+  ## Initailize item with a description, and not completed nor a priority
   def initialize(item_description)
     @description = item_description
     @completed_status = false
     @priority = false
   end
 
+  ## Check to see if item is flagged as a priority
   def priority?
   	@priority
   end
 
+  ## Check to see if item is flagged as completed
 	def item_completed?
   	@completed_status
   end
 
+  ## Flag item as a priority
   def make_priority
   	@priority = true
   end
 
+  ## Remove priority flag
   def remove_priority
   	@priority = false
   end
 
+  ## Flag item as complete
   def complete_item
   	@completed_status = true
   end
 
+  ## Remove complete flag for item
   def uncomplete_item
   	@completed_status = false
   end
 
+  ## Add due date to item using yy-mm-dd format
   def set_due_date(due_date)
   	@due_date = Date.parse due_date
   	@due_date = @due_date.strftime("%b %d, %Y")
 	end
 
+	## Remove due date from item
 	def remove_date
 		@due_date = nil
 	end
 
+	## Check to see if item has a due date.
 	def is_date_set?
 		if @due_date != nil
 			return true
@@ -139,6 +160,7 @@ class Item
 		end
 	end
 
+	## Print method for item, using checkboxes to show complete flag
   def print_item(index)
   	check_box = item_completed? ? "[x]" : "[ ]"
   	output_string = "#{check_box} #{index} - #{@description}"
@@ -150,5 +172,4 @@ class Item
   	end
   	puts output_string
   end
-
 end
